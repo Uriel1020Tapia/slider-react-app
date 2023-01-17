5468e501e3c2a7bdcd8dca7e25bf20e831abd721
Uriel Tapia Avalos <uriel.tapia@inai.org.mx>
2023-01-17 15:31:37 -0600

    first commit

---
+++ src/components/SliderImage.js
@@ -0,0 +1,60 @@
+import First from "../assets/First.jpg";
+import Second from "../assets/john-towner-JgOeRuGD_Y4-unsplash.jpg";
+import Third from "../assets/kalen-emsley-Bkci_8qcdvQ-unsplash.jpg";
+import Dell from "../assets/dell-LXI5kqCdEcE-unsplash.jpg";
+import Anders from "../assets/anders.jpg";
+import Milad from "../assets/milad.jpg";
+import Richard from "../assets/richard.jpg";
+
+export default [
+  {
+    title: "First Slide",
+    description: "This is the first slider Image of our carousel",
+    urls: First,
+  },
+  {
+    title: "Second Slide",
+    description: "This is the second slider Image of our carousel",
+    urls: Second,
+  },
+  {
+    title: "Third Slide",
+    description: "This is the Third slider Image of our carousel",
+    urls: Third,
+  },
+  {
+    title: "Fourth Slide",
+    description: "This is the Fourth slider Image of our carousel",
+    urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(133).jpg",
+  },
+  {
+    title: "Fifth Slide",
+    description: "This is the Fifth slider Image of our carousel",
+    urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(147).jpg",
+  },
+  {
+    title: "Sixth Slide",
+    description: "This is the Sixth slider Image of our carousel",
+    urls: Dell,
+  },
+  {
+    title: "Seventh Slide",
+    description: "This is the Seventh slider Image of our carousel",
+    urls: "https://66.media.tumblr.com/5af3f8303456e376ceda1517553ba786/tumblr_o4986gakjh1qho82wo1_1280.jpg",
+  },
+  {
+    title: "Seventh Slide",
+    description: "This is the Seventh slider Image of our carousel",
+    urls: Anders,
+  },
+  {
+    title: "Seventh Slide",
+    description: "This is the Seventh slider Image of our carousel",
+    urls: Richard,
+  },
+  {
+    title: "Seventh Slide",
+    description: "This is the Seventh slider Image of our carousel",
+    urls: Milad,
+  },
+];
